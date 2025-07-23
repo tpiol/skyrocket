2a5de7c69c96f9b2a39c026c7341fb1cae280939
Tia Piol <tiarox02@yahoo.com>
2025-07-23 12:08:25 -0700

    build and use application controller

---
+++ controllers/applications.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = (express.Router);
+
+const User = require("../models/user.js");
+
+// we will build our routes here
+
+module.exports = router;
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const isSignedIn = require('./middleware/is-signed-in.js');
 const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require('./controllers/auth.js');
+const applicationsController = require('./controllers/applications.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -41,6 +42,7 @@ app.get('/', (req, res) => {
 
 app.use('/auth', authController);
 app.use(isSignedIn); // setting this one up after someone has a chance to sign in 
+app.use('/users/:userId/applications', applicationsController)
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
