425325e6e1599a45fd97b7ba3f5348fd5fa16fea
Tia Piol <tiarox02@yahoo.com>
2025-07-23 11:58:15 -0700

    add custom middleware to app

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 const authController = require('./controllers/auth.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
@@ -28,6 +31,8 @@ app.use(
   })
 );
 
+app.use(passUserToView); // pass that user info to an EJS template
+
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
@@ -35,6 +40,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/auth', authController);
+app.use(isSignedIn); // setting this one up after someone has a chance to sign in 
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
