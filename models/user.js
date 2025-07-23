8b161210118d6f4eb04837ba58c3f90cdc990f33
Tia Piol <tiarox02@yahoo.com>
2025-07-23 10:09:23 -0700

    define embedded application schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,27 @@
 const mongoose = require('mongoose');
 
+const applicationSchema = new mongoose.Schema({
+company : {
+  type: String, 
+  required: true,
+},
+title: {
+  type: String,
+  required: true,
+}, 
+notes: {
+  type: String, 
+}, 
+postingLink: {
+  type: String,
+}, 
+status: {
+  type: String, 
+  enum: ["interested", "applied", "interviewing", "rejected", "accepted"],
+  required: true,
+},
+});
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +31,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema],
 });
 
 const User = mongoose.model('User', userSchema);
