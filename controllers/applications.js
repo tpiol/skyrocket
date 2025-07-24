8631acb802ab339fa374ca51644b6ed1d6a7f53e
Tia Piol <tiarox02@yahoo.com>
2025-07-24 10:20:12 -0700

    build the create functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -22,4 +22,19 @@ router.get("/new", async (req, res) => {
 });
 
 
+// POST /applications
+router.post("/", async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id)
+        currentUser.applications.push(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (error) {
+        console.log(error);
+        res.redirect("/")
+    }
+});
+
+
+
 module.exports = router;                                        
