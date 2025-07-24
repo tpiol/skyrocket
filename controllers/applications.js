6bfbd126cdb45a3f1963cf03a6c778964ac25b69
Tia Piol <tiarox02@yahoo.com>
2025-07-24 12:26:51 -0700

    build a show page

--- controllers/applications.js
+++ controllers/applications.js
@@ -28,13 +28,27 @@ router.get("/new", async (req, res) => {
 // POST /applications
 router.post("/", async (req, res) => {
     try {
-        const currentUser = await User.findById(req.session.user._id)
+        const currentUser = await User.findById(req.session.user._id);
         currentUser.applications.push(req.body);
         await currentUser.save();
         res.redirect(`/users/${currentUser._id}/applications`);
     } catch (error) {
         console.log(error);
         res.redirect("/")
+    }
+});
+
+// GET controllers/applications.js
+router.get("/:applicationId", async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const application = currentUser.applications.id(req.params.applicationId);
+        res.render("applications/show.ejs", {
+            application: application
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect("/");
     }
 });
 
